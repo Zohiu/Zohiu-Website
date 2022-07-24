7a7e2efb3d53bdb2f4af6ef3775ceb5d34266c47
Zohiu <zohiu3@gmail.com>
2022-07-24 14:05:17 +0200

    Added discord embed

---
+++ js/discordembed.js
@@ -0,0 +1,9 @@
+document.write(`
+
+    <meta content="Zohiu" property="og:title" />
+    <meta content="Site Description" property="og:description" />
+    <meta content="https://zohiu.de/" property="og:url" />
+    <meta content="https://zohiu.de/assets/embed.png" property="og:image" />
+    <meta content="#FF61A3" data-react-helmet="true" name="theme-color" />
+
+`);
