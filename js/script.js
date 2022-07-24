e17b06639a109e8258420416c6f52f54c311e618
Zohiu <zohiu3@gmail.com>
2022-07-24 13:38:36 +0200

    I hope this works

--- js/script.js
+++ js/script.js
@@ -1,25 +1,27 @@
-var z, i, elmnt, file, xhttp;
-/* Loop through a collection of all HTML elements: */
-z = document.getElementsByTagName("*");
-for (i = 0; i < z.length; i++) {
-  elmnt = z[i];
-  /*search for elements with a certain atrribute:*/
-  file = elmnt.getAttribute("include-html");
-  if (file) {
-    /* Make an HTTP request using the attribute value as the file name: */
-    xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4) {
-        if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-        if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-        /* Remove the attribute, and call this function once more: */
-        elmnt.removeAttribute("include-html");
-        includeHTML();
-      }
-    }
-    xhttp.open("GET", file, true);
-    xhttp.send();
-    /* Exit the function: */
-    return;
-  }
-}
+document.write(`
+
+    <p style="text-align:left;">
+        <a href="https://zohiu.de"><img src="https://zohiu.de/assets/logo.png" width="50" height="50" style="border-radius: 10px;" class="text"></img></a>
+
+        <span style="float:right;" class="text gradient">
+            <a href="https://github.com/Zohiu">Github</a>
+        </span>
+
+        <span style="float:right;" class="text">
+            <text class="text">&nbsp|&nbsp</text>
+        </span>
+
+        <span style="float:right;" class="text gradient">
+            <a href="https://zohiu.de/archive">Archived Projects</a>
+        </span>
+
+        <span style="float:right;" class="text">
+            <text class="text">&nbsp|&nbsp</text>
+        </span>
+
+        <span style="float:right;" class="text gradient">
+            <a href="https://zohiu.de/projects">Projects</a>
+        </span>
+    </p>
+
+`);
